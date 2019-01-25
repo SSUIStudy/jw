77c642d681696a503d1b0b74f5b82795bdddd84e
shera9961 <shera9961@gmail.com>
2019-01-25 16:05:14 +0900

    gulpfils.js ㅡãmodify

--- Sample/gulpfile.js
+++ Sample/gulpfile.js
@@ -1,230 +1,143 @@
-/**
- * -------------------------------
- * 모듈 호출
- * -------------------------------
- */
-var gulp = require('gulp'),
-    uglify = require('gulp-uglify'),
-    concatCss = require('gulp-concat-css'),
-    minifycss = require('gulp-minify-css'),
-    rename  = require('gulp-rename'),
-    htmlmin = require('gulp-htmlmin'),
-    sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    sassdoc = require('sassdoc'),
-    imagemin  = require('gulp-imagemin'),
-    //spritesmith  = require('gulp.spritesmith'),
-    sourcemaps = require('gulp-sourcemaps'),
-    merge = require('merge-stream'),
-    del = require('del'),
-    browserSync  = require('browser-sync').create(),
-    reload = browserSync.reload;
+//https://github.com/Alecaddd/gulp-es6/blob/master/gulpfile.js
 
-/**
- * -------------------------------
- * 경로 담은 객체
- * -------------------------------
- */
-const PROJECTNAME = 'public';
-const DIR = {
-  SRC   : PROJECTNAME + '/src',
-  DIST  : PROJECTNAME + '/dist'
-};
-const SRC = {
-  JS    : DIR.SRC + '/js/**/*.js',
-  SASS   : DIR.SRC + '/stylesheets/**/*.scss',
-  CSS   : DIR.SRC + '/stylesheets/**/*.css',
-  HTML  : DIR.SRC + '/html/**/*.html',
-  IMAGES: DIR.SRC + '/images/**/*',
-  FONT : DIR.SRC + '/fonts/*'
-};
-const DIST = {
-  JS    : DIR.DIST + '/js',
-  CSS   : DIR.DIST + '/css', 
-  HTML  : DIR.DIST + '/html',
-  IMAGES: DIR.DIST + '/images',
-  FONT : DIR.DIST + '/fonts'
+// Load Gulp...of course
+const { src, dest, task, watch, series, parallel } = require('gulp');
+
+// CSS related plugins
+var sass         = require( 'gulp-sass' );
+var autoprefixer = require( 'gulp-autoprefixer' );
+
+// JS related plugins
+var uglify       = require( 'gulp-uglify' );
+var babelify     = require( 'babelify' );
+var browserify   = require( 'browserify' );
+var source       = require( 'vinyl-source-stream' );
+var buffer       = require( 'vinyl-buffer' );
+var stripDebug   = require( 'gulp-strip-debug' );
+
+// Utility plugins
+var rename       = require( 'gulp-rename' );
+var sourcemaps   = require( 'gulp-sourcemaps' );
+var notify       = require( 'gulp-notify' );
+var plumber      = require( 'gulp-plumber' );
+var options      = require( 'gulp-options' );
+var gulpif       = require( 'gulp-if' );
+
+// Browers related plugins
+var browserSync  = require( 'browser-sync' ).create();
+
+// Project related variables
+var styleSRC     = './src/stylesheets/**/*.scss';
+var styleURL     = './dist/css/';
+var mapURL       = './';
+
+var jsSRC        = './src/js/';
+var jsFront      = 'main.js';
+var jsFiles      = [ jsFront ];
+var jsURL        = './dist/js/';
+
+var imgSRC       = './src/images/**/*';
+var imgURL       = './dist/images/';
+
+var fontsSRC     = './src/fonts/**/*';
+var fontsURL     = './dist/fonts/';
+
+var htmlSRC     = './src/**/*.html';
+var htmlURL     = './dist/';
+
+var styleWatch   = './src/stylesheets/**/*.scss';
+var jsWatch      = './src/js/**/*.js';
+var imgWatch     = './src/images/**/*.*';
+var fontsWatch   = './src/fonts/**/*.*';
+var htmlWatch    = './src/**/*.html';
+
+// Tasks
+function browser_sync() {
+	browserSync.init({
+		server: {
+            baseDir: './dist/',
+            directory : true
+        },
+        port : 8080
+	});
 }
 
-/**
- * -------------------------------
- * config : 각 플러그인 옵션 설정
- * -------------------------------
- */
-var config = {
-  // Reference : https://github.com/kangax/html-minifier#options-quick-reference
-  'html': {
-    /**
-     * outputStyle (Type : Boolean  , Default : false)
-     * CSS의 컴파일 결과 코드스타일 지정
-     */
-    collapseWhitespace: 'true'
-    /**
-     * outputStyle (Type : Boolean  , Default : false)
-     * 스타일 요소 및 스타일 속성에서 css 축소(clean-css 사용)
-     */
-    //minifyCSS: 'true',
-    /**
-     * outputStyle (Type : Boolean  , Default : false)
-     * 스크립트 요소 및 이벤트 속성에서 javascript 최소화(uglify js 사용)
-     */
-    //minifyJS: 'true'
-  },
-  // Reference : https://github.com/sass/node-sass#options
-  'sass': {
-    /**
-     * outputStyle (Type : String  , Default : nested)
-     * CSS의 컴파일 결과 코드스타일 지정
-     * Values : nested, expanded, compact, compressed
-     */
-    outputStyle: 'expanded'
-    /**
-     * indentType (>= v3.0.0 , Type : String , Default : space)
-     * 컴파일 된 CSS의 "들여쓰기" 의 타입
-     * Values : space , tab
-     */
-    // indentType: 'space',
-    /**
-     * indentWidth (>= v3.0.0, Type : Integer , Default : 2)
-     * 컴파일 된 CSS의 "들여쓰기" 의 갯수
-     */
-    // indentWidth: 2,
-    /**
-     * precision (Type :  Integer , Default : 5)
-     * 컴파일 된 CSS의 소수점 자리수.
-     */
-    // precision: 5,
-    /**
-     * sourceComments (Type : Boolean , Default : false)
-     * 컴파일 된 CSS에 원본소스의 위치와 줄수 주석표시.
-     */
-    // sourceComments: false
-  },
-  // Reference : https://github.com/postcss/autoprefixer#options
-  // 브라우저 리스트 참고 : https://github.com/ai/browserslist#queries
-  'autoprefixer': {
-    // 지원 브라우저 체크
-    browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
-  },
-  'sassdoc': {
-    verbose: true
-  },
-  // Browser-sync
-  // Reference: http://www.browsersync.io/docs/options/
-	'browserSync': { 
-		server  : {
-      baseDir   : DIR.DIST,
-      directory : true
-    },
-    port    : 8080,
-    // Don't show any notifications in the browser.
-		notify  : false
-	}
+function reload(done) {
+	browserSync.reload();
+	done();
 }
 
-gulp.task('font', function() {
-  return gulp.src(SRC.FONT)
-             .pipe(gulp.dest(DIST.FONT));
-});
-/**
- * -------------------------------
- * task : HTML | html minify
- * -------------------------------
- */
-gulp.task('html', function() {
-  return gulp.src(SRC.HTML)
-             //.pipe(htmlmin(config.html))
-             .pipe(gulp.dest(DIST.HTML))
-             .pipe(reload({stream: true}));
-});
+function css(done) {
+	src( [ styleSRC ] )
+		.pipe( sourcemaps.init() )
+		.pipe( sass({
+			errLogToConsole: true,
+			outputStyle: 'expanded'
+		}) )
+		.on( 'error', console.error.bind( console ) )
+		.pipe( autoprefixer({ browsers: [ 'last 2 versions', '> 5%', 'Firefox ESR' ] }) )
+		//.pipe( rename( { suffix: '.min' } ) )
+		.pipe( sourcemaps.write( mapURL ) )
+		.pipe( dest( styleURL ) )
+		.pipe( browserSync.stream() );
+	done();
+};
 
-/**
- * -------------------------------
- * task : JS
- * -------------------------------
- */
-gulp.task('js', function() {
-  return gulp.src(SRC.JS)
-             //.pipe(uglify())
-             //.pipe(rename({suffix: '.min'}))
-             .pipe(gulp.dest(DIST.JS))
-             .pipe(reload({stream: true}));
-});
+function js(done) {
+	jsFiles.map( function( entry ) {
+		return browserify({
+			entries: [jsSRC + entry]
+		})
+		.transform( babelify, { presets: [ '@babel/preset-env' ] } )
+		.bundle()
+		.pipe( source( entry ) )
+		.pipe( rename( {
+			extname: '.min.js'
+        } ) )
+		.pipe( buffer() )
+		.pipe( gulpif( options.has( 'production' ), stripDebug() ) )
+		.pipe( sourcemaps.init({ loadMaps: true }) )
+		.pipe( uglify() )
+		.pipe( sourcemaps.write( '.' ) )
+		.pipe( dest( jsURL ) )
+		.pipe( browserSync.stream() );
+	});
+	done();
+};
 
-/**
- * -------------------------------
- * task : CSS | sass 컴파일, sassdoc, sourcemap
- * -------------------------------
- */
-gulp.task('css', function() {
-  return gulp.src(SRC.CSS)
-             //.pipe(concatCss('./common/common.css'))
-             //.pipe(minifycss())
-             .pipe(gulp.dest(DIST.CSS));
-});
+function triggerPlumber( src_file, dest_file ) {
+	return src( src_file )
+		.pipe( plumber() )
+		.pipe( dest( dest_file ) );
+}
 
-gulp.task('sass',gulp.series('css', function() {
-  return gulp.src(SRC.SASS)
-             .pipe(sourcemaps.init())
-             .pipe(sassdoc(config.sassdoc))
-             .pipe(sass(config.sass).on('error', sass.logError))
-             .pipe(autoprefixer(config.autoprefixer))
-             .pipe(sourcemaps.write('../maps'))
-             .pipe(gulp.dest(DIST.CSS))
-             //.pipe(concatCss('./pages/common.css'))
-             //.pipe(minifycss())
-             .pipe(reload({stream: true}));
-}));
-/**
- * -------------------------------
- * task : IMAGE
- * -------------------------------
- */
-// task : IMG 파일 minify해서 dist 폴더에 넣기 
-gulp.task('images'/*, ['sprite']*/, function() {
-  return gulp.src([SRC.IMAGES, '!' + DIR.SRC + '/images/sprites/*'])
-             .pipe(imagemin())
-			       .pipe(gulp.dest(DIST.IMAGES));
-});
+function images() {
+	return triggerPlumber( imgSRC, imgURL );
+};
 
-// task : 스프라이트 이미지 생성
-/**
-gulp.task('sprite', function() {
-	var spriteData = gulp.src(DIR.SRC + '/images/sprites/*.png')
-                       .pipe( spritesmith({
-                         'imgName' : '../images/sprites/sprite.png',
-                         'cssName' : '_sprite.scss'
-                       }) );
+function fonts() {
+	return triggerPlumber( fontsSRC, fontsURL );
+};
 
-	// return spriteData.pipe( gulp.dest('dist') );
-	var imgStream = spriteData.img.pipe(gulp.dest(DIST.IMAGES));
-	var cssStream = spriteData.css.pipe(gulp.dest(DIR.SRC + '/stylesheets'));
+function html() {
+	return triggerPlumber( htmlSRC, htmlURL );
+};
 
-	return merge(imgStream, cssStream);
-});
-*/
-// task : clean | 이전 dist 폴더의 파일 삭제
-gulp.task('clean', function() {
- return del([DIR.DIST]);
-});
+function watch_files() {
+	watch(styleWatch, series(css, reload));
+	watch(jsWatch, series(js, reload));
+	watch(imgWatch, series(images, reload));
+	watch(fontsWatch, series(fonts, reload));
+	watch(htmlWatch, series(html, reload));
+	/*src(jsURL + 'main.min.js')
+		.pipe( notify({ message: 'Gulp is Watching, Happy Coding!' }) );*/
+}
 
+task("css", css);
+task("js", js);
+task("images", images);
+task("fonts", fonts);
+task("html", html);
+task("default", parallel(css, images, fonts, html, browser_sync, watch_files));
 
-
-// task : browserSync
-gulp.task('browserSync', gulp.series('html', 'js', 'sass', function () { 
-  return browserSync.init(config.browserSync);
-}));
-
-// task : watch | 관찰할 업무 지정
-gulp.task('watch', function() {
-  gulp.watch(SRC.JS, ['js']);
-  gulp.watch(SRC.CSS, ['css']);
-  gulp.watch(SRC.SASS, ['sass']);
-  gulp.watch(SRC.HTML, ['html']);
-  gulp.watch(SRC.FONT, ['font']);
-  //gulp.watch([SRC.IMAGES, '!' + DIR.SRC + '/images/sprites/*'], ['images']);
-  // gulp.watch(DIR.SRC + '/images/sprites/*.png', ['sprite']);
-});
-
-// task : default | 'gulp'를 실행하면 sass, watch task를 실행함
-gulp.task('default', gulp.series('clean','browserSync', 'font', 'watch'));
+//task("watch", parallel(browser_sync, watch_files));
